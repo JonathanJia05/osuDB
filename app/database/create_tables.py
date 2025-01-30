bd1c5d6017dbdfd90538171833a6fea7d9e6e0ce
Jonathan Jia <jonathanj18456@gmail.com>
2025-01-30 17:06:37 -0500

    created postgresql table

--- app/database/create_tables.py
+++ app/database/create_tables.py
@@ -1,1 +1,33 @@
 import psycopg2
+from config import load_config
+
+
+def create_tables():
+    commands = [
+        """
+        CREATE TABLE beatmaps (
+            MapID INTEGER PRIMARY KEY,
+            MapsetID INTEGER NOT NULL,
+            Artist TEXT,
+            Title TEXT,
+            Difficulty TEXT,
+            Md5 TEXT UNIQUE,
+            Mode INTEGER,
+            Stars REAL
+        )
+        """
+    ]
+    try:
+        config = load_config()
+        with psycopg2.connect(**config) as conn:
+            with conn.cursor() as curr:
+                for command in commands:
+                    curr.execute(command)
+            conn.commit()
+            print("Tables successfully created")
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+
+if __name__ == "__main__":
+    create_tables()
