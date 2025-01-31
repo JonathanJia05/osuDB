47c97d1f6ab7e4e0b3627d352e0b044708b78a62
Jonathan Jia <jonathanj18456@gmail.com>
2025-01-30 19:09:16 -0500

    cleaned up code, removed redundant files

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from app.database.insert import insert
 
 
+# testing function
 def main():
     test_beatmap = (
-        2667,  # MapID
-        521,  # MapsetID
-        "Ootsuki Kenji",  # Artist
-        "Hitotoshite Jiku Ga Bureteiru",  # Title
-        "Insane!!",  # Difficulty
-        "14d001c1297e2f2fbe23b8463f437ff8",  # Md5
-        0,  # Mode
-        3.04,  # Stars
+        2667,
+        521,
+        "Ootsuki Kenji",
+        "Hitotoshite Jiku Ga Bureteiru",
+        "Insane!!",
+        "14d001c1297e2f2fbe23b8463f437ff8",
+        0,
+        3.04,
         "https://osu.ppy.sh/osu/2667",
     )
 
